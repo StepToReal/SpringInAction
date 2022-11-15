97d2775007ace772d7fd46d0835515013e63906a
steptoreal <jcob0920@gmail.com>
2022-11-15 23:48:38 +0900

    코드 설명 주석 추가 및 warning 제거

--- taco-cloud/src/main/java/tacos/HomeController.java
+++ taco-cloud/src/main/java/tacos/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "home"; // 뷰의 논리적인 이름 반환
+        return "home"; // 뷰의 논리적인 이름 반환 - resources/templates/ 아래 "이름".html 페이지를 호출하게 되어 있다.
     }
 }
