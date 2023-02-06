944aa6ba89ede499aecd855d0bbbfaa846d93456
steptoreal <jcob0920@gmail.com>
2023-02-07 00:49:33 +0900

    6장 REST 서비스 생성하기
    - REST Controller 기본 구현
    - 하이퍼 미디어 사용 기초 구현

---
+++ taco-cloud/src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,112 @@
+package tacos.web.api;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tacos.Order;
+import tacos.Taco;
+import tacos.data.OrderRepository;
+import tacos.data.TacoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+@RestController
+@RequestMapping(path = "/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+    private TacoRepository tacoRepo;
+    private OrderRepository orderRepo;
+
+    public DesignTacoController(TacoRepository tacoRepo, OrderRepository orderRepo) {
+        this.tacoRepo = tacoRepo;
+        this.orderRepo = orderRepo;
+    }
+
+    @GetMapping("/recent")
+    public CollectionModel<EntityModel<Taco>> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+
+        List<Taco> tacos = tacoRepo.findAll(page).getContent();
+        CollectionModel<EntityModel<Taco>> recentResources = CollectionModel.wrap(tacos);
+
+//        recentResources.add(Link.of("http://localhost:8080/design/recent", "recents"));
+
+//        recentResources.add(linkTo(DesignTacoController.class)
+//                .slash("recent")
+//                .withRel("recents"));
+
+        recentResources.add(linkTo(methodOn(DesignTacoController.class).recentTacos()).withRel("recents"));
+
+        return recentResources;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
+    @PutMapping("/{orderId}")
+    public Order putOrder(@RequestBody Order order) {
+        return orderRepo.save(order);
+    }
+
+    @PatchMapping(value = "/{orderId}", consumes = "applicaiton/json")
+    public Order patchOrder(@PathVariable("orderId") Long orderId, @RequestBody Order patch) {
+        Order order = orderRepo.findById(orderId).get();
+
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+
+        return orderRepo.save(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable("orderId") Long orderId) {
+        try {
+            orderRepo.deleteById(orderId);
+        } catch (EmptyResultDataAccessException e) {}
+    }
+}
