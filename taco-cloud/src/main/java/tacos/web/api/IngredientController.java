a46f5a07c39fa1a1f7bf94d158a6453e28ae5d7a
steptoreal <jcob0920@gmail.com>
2023-03-27 23:15:30 +0900

    6장 HATEOAS 관련 rework
    - 반환 model 및 Assembler 생성
    - 반환 model 이름 고정
    - 스프링 데이터 REST 사용
    - 커스텀 엔드포인트 추가

--- taco-cloud/src/main/java/tacos/web/api/IngredientController.java
+++ taco-cloud/src/main/java/tacos/web/api/IngredientController.java
@@ -21,39 +21,39 @@ public class IngredientController {
         this.repo = repo;
     }
 
-    @GetMapping
-    public Iterable<Ingredient> allIngredients() {
-        return repo.findAll();
-    }
+//    @GetMapping
+//    public Iterable<Ingredient> allIngredients() {
+//        return repo.findAll();
+//    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Ingredient> getIngredientById(@PathVariable("id") String id) {
-        Ingredient ingredient = repo.findById(id).orElse(null);
-        return new ResponseEntity<>(ingredient, HttpStatus.OK);
-    }
-
-    @PutMapping(path = "/{id}")
-    public ResponseEntity<Ingredient> putIngredient(@PathVariable("id") String id, @RequestBody Ingredient ingredient) {
-        Optional<Ingredient> origin = repo.findById(id);
-
-        if (origin.isPresent()) {
-            origin.get().setName(ingredient.getName());
-            repo.save(origin.get());
-            return new ResponseEntity<>(origin.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-        }
-    }
-
-    @PostMapping()
-    public ResponseEntity<Ingredient> postIngredient(@RequestBody Ingredient ingredient) {
-        Ingredient savedIngredient = repo.save(ingredient);
-
-        return new ResponseEntity<>(savedIngredient, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteIngredient(@PathVariable("id") String id) {
-        repo.deleteById(id);
-    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Ingredient> getIngredientById(@PathVariable("id") String id) {
+//        Ingredient ingredient = repo.findById(id).orElse(null);
+//        return new ResponseEntity<>(ingredient, HttpStatus.OK);
+//    }
+//
+//    @PutMapping(path = "/{id}")
+//    public ResponseEntity<Ingredient> putIngredient(@PathVariable("id") String id, @RequestBody Ingredient ingredient) {
+//        Optional<Ingredient> origin = repo.findById(id);
+//
+//        if (origin.isPresent()) {
+//            origin.get().setName(ingredient.getName());
+//            repo.save(origin.get());
+//            return new ResponseEntity<>(origin.get(), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+//        }
+//    }
+//
+//    @PostMapping()
+//    public ResponseEntity<Ingredient> postIngredient(@RequestBody Ingredient ingredient) {
+//        Ingredient savedIngredient = repo.save(ingredient);
+//
+//        return new ResponseEntity<>(savedIngredient, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void deleteIngredient(@PathVariable("id") String id) {
+//        repo.deleteById(id);
+//    }
 }
--- taco-cloud/src/main/java/tacos/web/api/TacoResource.java
+++ taco-cloud/src/main/java/tacos/web/api/TacoResource.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
-import tacos.Ingredient;
 import tacos.Taco;
 
 import java.util.Date;
-import java.util.List;
 
+@Relation(value="taco", collectionRelation="tacos")
 public class TacoResource extends RepresentationModel<TacoResource> {
+
+    private static final IngredientResourceAssembler ingredientAssembler = new IngredientResourceAssembler();
 
     @Getter
     private final String name;
@@ -19,11 +20,11 @@ public class TacoResource extends RepresentationModel<TacoResource> {
     private final Date createdAt;
 
     @Getter
-    private final List<Ingredient> ingredients;
+    private final CollectionModel<IngredientResource> ingredients;
 
     public TacoResource(Taco taco) {
         this.name = taco.getName();
         this.createdAt = taco.getCreatedAt();
-        this.ingredients = taco.getIngredients();
+        this.ingredients = ingredientAssembler.toCollectionModel(taco.getIngredients());
     }
 }
