cc2f7fb8f11f15fed8b40d1c0c25a9db6e04b2e2
steptoreal <jcob0920@gmail.com>
2023-04-17 23:10:41 +0900

    통합 플로우 구현 프로젝트 init (taco-integration)

---
+++ taco-integration/src/main/java/tacos/TacoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoIntegrationApplication.class, args);
+	}
+
+}
