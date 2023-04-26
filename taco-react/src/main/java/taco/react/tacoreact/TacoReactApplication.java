4ac5a892e982ebbafef322e009b2618897e168f9
steptoreal <jcob0920@gmail.com>
2023-04-26 22:41:24 +0900

    taco react study project init

---
+++ taco-react/src/main/java/taco/react/tacoreact/TacoReactApplication.java
@@ -0,0 +1,13 @@
+package taco.react.tacoreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoReactApplication.class, args);
+    }
+
+}
---
+++ taco-react/src/test/java/taco/react/tacoreact/TacoReactApplicationTests.java
@@ -0,0 +1,13 @@
+package taco.react.tacoreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
